a36cba9e7645f9e0e52821b40b343b56c7b3e388
Jake <unknownhybrid@gmail.com>
2013-03-02 12:03:27 -0600

    hmhmhm

--- Pong/Pong/PongProject/MainScene.cs
+++ Pong/Pong/PongProject/MainScene.cs
@@ -28,7 +28,7 @@ namespace PongProject
         private const string PADDLE_FILENAME = "Content/paddle.wpk";
         private const string BALL_FILENAME = "Content/ball.wpk";
         private const string WALL_FILENAME = "Content/wall.wpk";
-
+        
         private int score = 0;
 
         protected override void CreateScene()
@@ -51,7 +51,7 @@ namespace PongProject
                 .AddComponent(new BallBehavior(scoreText));
             
             Entity leftPaddle = this.CreateSquareSprite("Left Paddle", LEFT_PADDLE_STARTING_X, PADDLE_STARTING_Y, PADDLE_FILENAME, true).AddComponent(new LeftPaddleBehavior());
-            Entity rightPaddle = this.CreateSquareSprite("Right Paddle", RIGHT_PADDLE_STARTING_X, PADDLE_STARTING_Y, PADDLE_FILENAME, true).AddComponent(new RightPaddleBehavior()); //ComputerPaddleBehavior(ball.FindComponentOfType<RigidBody2D>()));
+            Entity rightPaddle = this.CreateSquareSprite("Right Paddle", RIGHT_PADDLE_STARTING_X, PADDLE_STARTING_Y, PADDLE_FILENAME, true).AddComponent(new ComputerPaddleBehavior(ball.FindComponentOfType<RigidBody2D>()));//new RightPaddleBehavior()); //
 
             Entity topWall = this.CreateSquareSprite("Top Wall", 400, 0, WALL_FILENAME, true);
             Entity bottomWall = this.CreateSquareSprite("Bottom Wall", 400, 600, WALL_FILENAME, true);
